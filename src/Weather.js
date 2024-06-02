a8b3e015803fce5fd3c284e2e31b4cceafd419bb
teimoor123 <teimoor003@icloud.com>
2024-06-02 14:57:47 -0400

    design changes 4

--- src/Weather.js
+++ src/Weather.js
@@ -2,9 +2,32 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+function capitalize(str) {
+  return str.replace(/\b\w/g, function(char) {
+    return char.toUpperCase();
+  });
+}
+
 const Weather = ({ city }) => {
   const [weather, setWeather] = useState(null);
   const apiKey = 'a457c6861e66605682b30bc5b79b865c';
+  const weatherIcons = {
+    Thunderstorm: "https://cdn-icons-png.flaticon.com/512/3104/3104612.png",
+    Drizzle: "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather05-512.png",
+    Rain: "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather07-512.png",
+    Snow: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Weather-snow.svg/1024px-Weather-snow.svg.png",
+    Mist: "https://png.pngtree.com/png-vector/20220621/ourmid/pngtree-daytime-foggy-weather-clouds-illustration-png-image_5246770.png",
+    Smoke: "https://cdn4.iconfinder.com/data/icons/weather-708/64/45_fog_cloud_weather_smoke_smog_transparent_wintry-512.png",
+    Haze: "https://cdn-icons-png.flaticon.com/512/1779/1779807.png",
+    Dust: "https://icons.veryicon.com/png/o/weather/weather-7/dust-1.png",
+    Fog: "https://cdn4.iconfinder.com/data/icons/weather-708/64/45_fog_cloud_weather_smoke_smog_transparent_wintry-512.png",
+    Sand: "https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-19-512.png",
+    Ash: "https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-19-512.png",
+    Squall: "https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-19-512.png",
+    Tornado: "https://cdn-icons-png.flaticon.com/512/6566/6566398.png",
+    Clear: "https://www.onlygfx.com/wp-content/uploads/2018/09/4-clipart-sun-1.png",
+    Clouds: "https://cdn.iconscout.com/icon/free/png-256/free-cloudy-weather-11-1147979.png",
+  };
 
   useEffect(() => {
     const fetchWeather = async () => {
@@ -25,6 +48,9 @@ const Weather = ({ city }) => {
   }
 
   const { main, weather: weatherDetails, wind, clouds, sys, visibility, coord } = weather;
+  const weatherCondition = weather.weather[0].main;
+  const weatherIconUrl = weatherIcons[weatherCondition];
+  const cityDisplay = capitalize(city);
 
 
   return (
@@ -33,8 +59,14 @@ const Weather = ({ city }) => {
         <p>Last updated {new Date(weather.dt * 1000).toLocaleString()}</p>
       </div>
       <div className="weather-container">
-        <h1>{city}</h1>
-        <h2>{weather.main.temp}°C</h2>
+        <div className="weather-main">
+          <div className="weather-header">
+            <div className="weather-city">{cityDisplay}</div>
+            <br></br>
+            <h2>{weather.main.temp}°C</h2>
+          </div>
+          <img src={weatherIconUrl} alt={weatherCondition} height="150px" />
+        </div>
         <p>Feels Like: {weather.main.feels_like}°C</p>
         <p>Min Temperature: {weather.main.temp_min}°C</p>
         <p>Max Temperature: {weather.main.temp_max}°C</p>
