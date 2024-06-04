133afb43717db33861600691ed4a4f083ce611f3
teimoor123 <teimoor003@icloud.com>
2024-06-03 23:40:29 -0400

    design changes 5
    Edited time to match time zone. A few design changes

--- src/Weather.js
+++ src/Weather.js
@@ -25,7 +25,7 @@ const Weather = ({ city }) => {
     Ash: "https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-19-512.png",
     Squall: "https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-19-512.png",
     Tornado: "https://cdn-icons-png.flaticon.com/512/6566/6566398.png",
-    Clear: "https://www.onlygfx.com/wp-content/uploads/2018/09/4-clipart-sun-1.png",
+    Clear: "https://seeklogo.com/images/S/sunny-weather-symbol-logo-4E2A5E54AA-seeklogo.com.png",
     Clouds: "https://cdn.iconscout.com/icon/free/png-256/free-cloudy-weather-11-1147979.png",
   };
 
@@ -47,11 +47,30 @@ const Weather = ({ city }) => {
     return <p>Invalid city input!</p>;
   }
 
-  const { main, weather: weatherDetails, wind, clouds, sys, visibility, coord } = weather;
-  const weatherCondition = weather.weather[0].main;
-  const weatherIconUrl = weatherIcons[weatherCondition];
-  const cityDisplay = capitalize(city);
+  const { main, weather: weatherDetails, wind, clouds, sys, visibility, coord, timezone } = weather;
 
+  const localTime = (time) => {
+    const date = new Date((time + timezone) * 1000);
+    const offset = date.getTimezoneOffset() * 60;
+    return new Date((time + timezone + offset) * 1000).toLocaleTimeString();
+  }
+
+  const weatherCondition = weather.weather[0].main;
+  let weatherIconUrl = weatherIcons[weatherCondition];
+  const isNight = localTime >= sys.sunset || localTime < sys.sunrise;
+  const rainConditions = ['Rain', 'Drizzle', 'Thunderstorm', 'Mist'];
+  const isWetCondition = rainConditions.includes(weatherCondition);
+
+  if (isNight) {
+    if (isWetCondition) {
+      weatherIconUrl = "https://icons.veryicon.com/png/o/weather/weather-icon-7/rain-at-night-1.png"
+    }
+    else {
+      weatherIconUrl = "https://cdn-icons-png.flaticon.com/512/6805/6805164.png"
+    }
+  }
+
+  const cityDisplay = capitalize(city);
 
   return (
     <div className="result">
@@ -63,23 +82,37 @@ const Weather = ({ city }) => {
           <div className="weather-header">
             <div className="weather-city">{cityDisplay}</div>
             <br></br>
-            <h2>{weather.main.temp}°C</h2>
+            <div className="weather-temp">{Math.round(weather.main.temp)}°C</div>
           </div>
-          <img src={weatherIconUrl} alt={weatherCondition} height="150px" />
+          <img src={weatherIconUrl} alt={weatherCondition} height="200px" />
         </div>
         <p>Feels Like: {weather.main.feels_like}°C</p>
+        <hr className="transparent-hr" />
         <p>Min Temperature: {weather.main.temp_min}°C</p>
+        <hr className="transparent-hr" />
         <p>Max Temperature: {weather.main.temp_max}°C</p>
+        <hr className="transparent-hr" />
         <p>Condition: {weather.weather[0].main}</p>
+        <hr className="transparent-hr" />
         <p>Description: {weather.weather[0].description}</p>
+        <hr className="transparent-hr" />
         <p>Humidity: {weather.main.humidity}%</p>
+        <hr className="transparent-hr" />
         <p>Pressure: {weather.main.pressure} hPa</p>
+        <hr className="transparent-hr" />
         <p>Visibility: {weather.visibility / 1000} km</p>
+        <hr className="transparent-hr" />
         <p>Wind Speed: {weather.wind.speed} m/s</p>
+        <hr className="transparent-hr" />
         <p>Wind Direction: {weather.wind.deg}°</p>
+        <hr className="transparent-hr" />
         <p>Cloudiness: {weather.clouds.all}%</p>
-        <p>Sunrise: {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()}</p>
-        <p>Sunset: {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}</p>
+        <hr className="transparent-hr" />
+        <div className="weather-sun">
+          <p>Sunrise: {localTime(weather.sys.sunrise)}</p>
+          <p>Sunset: {localTime(weather.sys.sunset)}</p>
+        </div>
+        <hr className="transparent-hr" />
         <p>Coordinates: {weather.coord.lat}, {weather.coord.lon}</p>
       </div>
     </div>
